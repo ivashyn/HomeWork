c169ccfa8b34054de76de84b7e36e49bde80d447
ivashyn <ivashyn.vadym@gmail.com>
2017-09-12 21:50:04 +0300

    Added IdentityServer4 Resource Owner Password Flow

---
+++ Resource Owner Password Credentials/IdentityServer4WithCustomUserRepository/UserServices/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace CustomIdentityServer4.UserServices
+{
+    public class UserRepository : IUserRepository
+    {
+        // some dummy data. Replce this with your user persistence. 
+        private readonly List<CustomUser> _users = new List<CustomUser>
+        {
+            new CustomUser{
+                SubjectId = "123",
+                UserName = "damienbod",
+                Password = "damienbod",
+                Email = "damienbod@email.ch"
+            },
+            new CustomUser{
+                SubjectId = "124",
+                UserName = "vadym",
+                Password = "vadym",
+                Email = "ivashyn.vadym@gmail.com"
+            },
+        };
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            var user = FindByUsername(username);
+            if (user != null)
+            {
+                return user.Password.Equals(password);
+            }
+
+            return false;
+        }
+
+        public CustomUser FindBySubjectId(string subjectId)
+        {
+            return _users.FirstOrDefault(x => x.SubjectId == subjectId);
+        }
+
+        public CustomUser FindByUsername(string username)
+        {
+            return _users.FirstOrDefault(x => x.UserName.Equals(username, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
